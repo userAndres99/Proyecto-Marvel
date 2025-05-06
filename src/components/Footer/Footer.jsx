13e76de21c1ae8f1e096dfb0737e213bf1539f9b
Andres Menard <andresmenard.arg99@gmail.com>
2025-05-06 15:35:00 -0300

    asegurando de que el footer aparezca

--- src/components/Footer/Footer.jsx
+++ src/components/Footer/Footer.jsx
@@ -4,18 +4,16 @@ const Footer = () => {
   const { t } = useTranslation();
 
   return (
-    <footer class="bg-gray-800 text-gray-200 py-6 mt-10">
-  <div class="max-w-7xl mx-auto text-center">
-    <p class="text-sm">
-      © 2025 <span class="text-red-600 font-semibold">{t("proyectMarvel")}</span>.{t("rights")}
-    </p>
-    <p class="mt-2 text-xs italic text-gray-300">
-      {t("propiety")}
-    </p>
-  </div>
-</footer>
-
-
+    <footer className="relative z-10 bg-gray-800 text-gray-200 py-6 mt-10">
+      <div className="max-w-7xl mx-auto text-center">
+        <p className="text-sm">
+          © 2025 <span className="text-red-600 font-semibold">{t("proyectMarvel")}</span>.{t("rights")}
+        </p>
+        <p className="mt-2 text-xs italic text-gray-300">
+          {t("propiety")}
+        </p>
+      </div>
+    </footer>
   );
 };
 
