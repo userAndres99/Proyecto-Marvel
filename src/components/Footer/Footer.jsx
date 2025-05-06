15e3e39570135aae43e526cb73f8693bb9b2afca
gabigallardo <matias.gallardo@est.fi.uncoma.edu.ar>
2025-05-06 15:03:29 -0300

    estilos footer

--- src/components/Footer/Footer.jsx
+++ src/components/Footer/Footer.jsx
@@ -4,9 +4,18 @@ const Footer = () => {
   const { t } = useTranslation();
 
   return (
-    <footer className="relative z-10 flex items-center justify-center px-4 py-6 bg-gray-800 text-gray-300 min-h-[80px]">
-      <p>{t("rights")}</p>
-    </footer>
+    <footer class="bg-gray-800 text-gray-200 py-6 mt-10">
+  <div class="max-w-7xl mx-auto text-center">
+    <p class="text-sm">
+      © 2025 <span class="text-red-600 font-semibold">{t("proyectMarvel")}</span>.{t("rights")}
+    </p>
+    <p class="mt-2 text-xs italic text-gray-300">
+      {t("propiety")}
+    </p>
+  </div>
+</footer>
+
+
   );
 };
 
