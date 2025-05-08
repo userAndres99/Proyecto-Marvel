b8e69a12a76e6137a4ba0a787a152e06185e5fd0
Andres Menard <andresmenard.arg99@gmail.com>
2025-05-07 22:41:31 -0300

    algo que me estaba dando tok

--- src/pages/Home/Home.jsx
+++ src/pages/Home/Home.jsx
@@ -4,11 +4,6 @@ import { useTranslation } from "react-i18next";
 import { useEffect } from "react";
 import { ROUTES } from '../../const/routes';
 
-//---importacion de componentes
-import Boton from '../../components/Boton/Boton';
-//--------------------------------
-
-
 export default function Home() {
   const [hovered, setHovered] = useState(null);
   const navigate = useNavigate();
@@ -23,43 +18,43 @@ export default function Home() {
     <>
      
       <div className="flex h-screen mt-10">
-        {/* Mitad Heroes */}
+              {/* Mitad Heroes */}
         <div
-  onMouseEnter={() => setHovered('heroes')}
-  onMouseLeave={() => setHovered(null)}
-  onClick={() => navigate(`${ROUTES.Personajes}?heroe=true`)}
-  className="w-1/2 bg-cover bg-center relative flex justify-center transition-all duration-300 cursor-pointer hover:brightness-90"
-  style={{
-    backgroundImage: "url('/heroes.jpg')",
-    clipPath: 'polygon(0 0, 100% 0, 85% 100%, 0 100%)',
-    filter: hovered === 'villanos' ? 'blur(3px)' : 'none',
-  }}
->
-<div className="absolute top right-0 bg-red-700/80 px-10 py-3 rounded-lg shadow-lg w-full">
-  <h2 className="text-white text-6xl text-center font-bold">
-      {t("heroes")}
-    </h2>
-  </div>
-</div>
+          onMouseEnter={() => setHovered('heroes')}
+          onMouseLeave={() => setHovered(null)}
+          onClick={() => navigate(`${ROUTES.Personajes}?heroe=true`)}
+          className="w-1/2 bg-cover bg-center relative flex justify-center transition-all duration-300 cursor-pointer hover:brightness-90"
+          style={{
+            backgroundImage: "url('/heroes.jpg')",
+            clipPath: 'polygon(0 0, 100% 0, 85% 100%, 0 100%)',
+            filter: hovered === 'villanos' ? 'blur(3px)' : 'none',
+          }}
+        >
+          <div className="absolute top right-0 bg-red-700/80 px-10 py-3 rounded-lg shadow-lg w-full">
+            <h2 className="text-white text-6xl text-center font-bold">
+              {t("heroes")}
+            </h2>
+          </div>
+        </div>
 
-<div
-  onMouseEnter={() => setHovered('villanos')}
-  onMouseLeave={() => setHovered(null)}
-  onClick={() => navigate(`${ROUTES.Personajes}?heroe=false`)}
-  className="w-1/2 bg-cover bg-center relative flex justify-center transition-all duration-300 cursor-pointer hover:brightness-90"
-  style={{
-    backgroundImage: "url('/villanos.webp')",
-    clipPath: 'polygon(15% 0, 100% 0, 100% 100%, 0 100%)',
-    filter: hovered === 'heroes' ? 'blur(3px)' : 'none',
-  }}
->
-<div className="absolute top right-0 bg-red-700/80 px-10 py-3 rounded-lg shadow-lg w-full ">
-  <h2 className="text-white text-6xl text-center font-bold">
-    {t("villains")}
-  </h2>
-</div>
+        <div
+          onMouseEnter={() => setHovered('villanos')}
+          onMouseLeave={() => setHovered(null)}
+          onClick={() => navigate(`${ROUTES.Personajes}?heroe=false`)}
+          className="w-1/2 bg-cover bg-center relative flex justify-center transition-all duration-300 cursor-pointer hover:brightness-90"
+          style={{
+            backgroundImage: "url('/villanos.webp')",
+            clipPath: 'polygon(15% 0, 100% 0, 100% 100%, 0 100%)',
+            filter: hovered === 'heroes' ? 'blur(3px)' : 'none',
+          }}
+        >
+          <div className="absolute top right-0 bg-red-700/80 px-10 py-3 rounded-lg shadow-lg w-full ">
+            <h2 className="text-white text-6xl text-center font-bold">
+              {t("villains")}
+            </h2>
+          </div>
 
-</div>
+        </div>
 
 
       </div>
